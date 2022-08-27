da19b33bc84a34bc0673f3078bcd5776e50cf717
nattou-tamago <88647501+nattou-tamago@users.noreply.github.com>
2022-08-27 15:05:51 +0900

    show map

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
 
+            <div class="col-12 mb-3">
+                <div id='map' style="height: 450px;"></div>
+            </div>
+
             @forelse($courses as $course)
 
                 @include('courses.partials.course', [])
@@ -12,4 +16,11 @@
                 <p>登録されたランニングコースはありません。</p>
             @endforelse
 
+@endsection
+
+@section('scripts')
+    <script>
+        const mapToken = "{{ env('MAPBOX_TOKEN') }}";
+    </script>
+    <script src="{{ asset('js/clusterMap.js') }}"></script>
 @endsection
